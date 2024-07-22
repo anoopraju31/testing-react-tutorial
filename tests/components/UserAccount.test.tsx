bf28f457903671d99aeb2b22f87a53b186f035d0
anoopraju31 <anoopraju31@gmail.com>
2024-07-23 00:09:58 +0530

    User Account Component testing added

---
+++ tests/components/UserAccount.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import UserAccount from '../../src/components/UserAccount'
+import { User } from '../../src/entities'
+
+describe('User Account', () => {
+	it("Should render user's name", () => {
+		const user: User = { id: 1, name: 'Anoop' }
+
+		render(<UserAccount user={user} />)
+
+		expect(screen.getByText(user.name)).toBeInTheDocument()
+	})
+
+	it('Should render the edit button if the user is an admin', () => {
+		const user: User = { id: 1, name: 'Anoop', isAdmin: true }
+
+		render(<UserAccount user={user} />)
+
+		const button = screen.getByRole('button')
+
+		expect(button).toBeInTheDocument()
+		expect(button).toHaveTextContent(/edit/i)
+		expect(screen.getByText(user.name))
+	})
+
+	it('Should not render edit button if user is not an admin', () => {
+		const user: User = { id: 2, name: 'Anoop', isAdmin: false }
+
+		render(<UserAccount user={user} />)
+
+		const button = screen.queryByRole('button')
+
+		expect(button).not.toBeInTheDocument()
+		expect(screen.getByText(user.name))
+	})
+})
